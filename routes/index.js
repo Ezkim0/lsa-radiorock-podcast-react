479b8a1f82d66fe041291b619f6cef809be01418
Matti Murtonen <matti@murtonen.org>
2015-09-29 23:02:00 +0300

    _ updated

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
-var express = require('express'),
-mongoose = require('mongoose');
+var express = require('express');
+var mongoose = require('mongoose');
 
 var router = express.Router();
 
@@ -9,9 +9,21 @@ var router = express.Router();
   res.json({ message: 'hooray! welcome to our api!' });
 });*/
 
-router.get('/podcasts', function(req, res, next) {
-  res.json({ message: 'podcasts' });
+router.get('/podcasts/all', function(req, res, next) {
+  //res.json({ message: 'podcasts' });
+ 
   // get podcasts
+  var Rrpodcasts = mongoose.model('Rrpodcasts');
+  Rrpodcasts
+    .find({})
+    .sort({'created_at' : -1})
+    .exec(function(err, posts) {
+      if(err) {
+        console.log(err);
+        return;
+      }
+      res.json(posts);
+    });
 
 });    
 
